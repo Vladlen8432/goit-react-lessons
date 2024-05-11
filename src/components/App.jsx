ad08b6c2e7cf2f50a0259d71934ce7d26e98a2d6
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-05-12 00:58:19 +0300

    checkpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,60 @@
+const productData = [
+  {
+    id: '1',
+    title: 'Tacos with cheese',
+    price: 10.9,
+    discount: null,
+    imageURL:
+      'https://www.oliveandmango.com/images/uploads/2022_02_21_cheesy_baked_tacos_1.jpg',
+  },
+
+  {
+    id: '2',
+    title: 'Tacos with lime',
+    price: 12.6,
+    discount: 1.5,
+    imageURL:
+      'https://michielskitchen.com/wp-content/uploads/2020/08/lime-chicken-and-coriander-tacos-recipe-michiels-kitchen-01-1.jpg',
+  },
+
+  {
+    id: '3',
+    title: 'Tacos with pineapple',
+    price: 13.4,
+    discount: 2.5,
+    imageURL:
+      'https://michielskitchen.com/wp-content/uploads/2020/08/lime-chicken-and-coriander-tacos-recipe-michiels-kitchen-01-1.jpg',
+  },
+];
+
 export const App = () => {
   return (
-    <>
+    <section>
       <h1>LOGO</h1>
-      <button type="button" className="myButton">
-        BUTTON
-      </button>
-      <p>Here will be text</p>
-    </>
+
+      {productData.map(product => {
+        return (
+          <Product
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            discount={product.discount}
+            imageURL={product.imageURL}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+const Product = ({ title, price, imageURL, discount }) => {
+  return (
+    <div>
+      <img src={imageURL} alt="Tacos With Lime" width="640" />
+      <h2>{title}</h2>
+      {discount && <h3>Discount: {discount}$</h3>}
+      <p>{price}$</p>
+      <button type="button">Add to cart</button>
+    </div>
   );
 };
