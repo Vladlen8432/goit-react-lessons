fa12211e4a0ba17dbe604fd464af2f8529d76753
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-06-09 16:31:33 +0300

    module03_lesson02_finished

--- src/components/AppWithRequest.jsx
+++ src/components/AppWithRequest.jsx
@@ -15,48 +15,34 @@ export default class AppWithRequest extends Component {
 
   fetchPosts = async () => {
     try {
-      this.setState({
-        isLoading: true,
-      });
+      this.setState({ isLoading: true });
       const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts`
+        'https://jsonplaceholder.typicode.com/posts'
       );
-      this.setState({
-        posts: data,
-      });
+      this.setState({ posts: data });
     } catch (error) {
       this.setState({ error: error.message });
     } finally {
-      this.setState({
-        isLoading: false,
-      });
+      this.setState({ isLoading: false });
     }
   };
 
   fetchPostsComments = async () => {
     try {
-      this.setState({
-        isLoading: true,
-      });
+      this.setState({ isLoading: true });
       const { data } = await axios.get(
         `https://jsonplaceholder.typicode.com/comments?postId=${this.state.selectedPostId}`
       );
-      this.setState({
-        comments: data,
-      });
+      this.setState({ comments: data });
     } catch (error) {
       this.setState({ error: error.message });
     } finally {
-      this.setState({
-        isLoading: false,
-      });
+      this.setState({ isLoading: false });
     }
   };
 
   onSelectPostId = postId => {
-    this.setState({
-      selectedPostId: postId,
-    });
+    this.setState({ selectedPostId: postId });
   };
 
   componentDidMount() {
@@ -64,7 +50,7 @@ export default class AppWithRequest extends Component {
   }
 
   componentDidUpdate(_, prevState) {
-    if (prevState.selectedPostId !== this.setState.selectedPostId) {
+    if (prevState.selectedPostId !== this.state.selectedPostId) {
       this.fetchPostsComments();
     }
   }
@@ -79,61 +65,45 @@ export default class AppWithRequest extends Component {
           </p>
         )}
         {this.state.isLoading && <Loader />}
-
         <div className={css.listWrapper}>
           <ul className={css.postList}>
             {this.state.posts !== null &&
-              this.state.posts.map(post => {
-                return (
-                  <li
-                    key={post.id}
-                    onClick={() => this.onSelectPostId(post.id)}
-                    className={css.postListItem}
-                  >
-                    <h2 className={css.itemTitle}>{post.title}</h2>
-                    <p className={css.itemBody}>
-                      <b>Body: </b>
-                      {post.body}
-                    </p>
-                  </li>
-                );
-              })}
+              this.state.posts.map(post => (
+                <li
+                  key={post.id}
+                  onClick={() => this.onSelectPostId(post.id)}
+                  className={css.postListItem}
+                >
+                  <h2 className={css.itemTitle}>{post.title}</h2>
+                  <p className={css.itemBody}>
+                    <b>Body: </b>
+                    {post.body}
+                  </p>
+                </li>
+              ))}
           </ul>
 
           <ul className={css.commentsList}>
-            <li className={css.commentsListItem}>
-              Selected post id: {this.state.selectedPostId}
-            </li>
-            <li className={css.commentsListItem}>
-              <h2 className={css.commentTitle}>Id labore ex et quam laborum</h2>
-              <h3 className={css.commentEmail}>Email: Eliseo@gardner.biz</h3>
-              <p className={css.commentBody}>
-                <b>Body: </b>
-                Laudantium enim quasi est quidem magnam voluptate ipsam
-                eos\ntempora quo necessitatibus\ndolor quam autem
-                quasi\nreiciendis et nam sapiente accusantium
-              </p>
-            </li>
+            {this.state.selectedPostId && (
+              <li className={css.commentsListItem}>
+                Selected post id: {this.state.selectedPostId}
+              </li>
+            )}
+            {!this.state.isLoading &&
+              this.state.comments !== null &&
+              this.state.comments.map(comment => (
+                <li key={comment.id} className={css.commentsListItem}>
+                  <h2 className={css.commentTitle}>{comment.name}</h2>
+                  <h3 className={css.commentEmail}>Email: {comment.email}</h3>
+                  <p className={css.commentBody}>
+                    <b>Body: </b>
+                    {comment.body}
+                  </p>
+                </li>
+              ))}
           </ul>
         </div>
       </div>
     );
   }
 }
-
-// [
-//     {
-//       "userId": 1,
-//       "id": 1,
-//       "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//       "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-//     },]
-
-// [
-//   {
-//     "postId": 1,
-//     "id": 1,
-//     "name": "id labore ex et quam laborum",
-//     "email": "Eliseo@gardner.biz",
-//     "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-//   },]
